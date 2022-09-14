6721120fd2408326679ec261f6c1aee6bb3268c9
Seong, Hyunwoo <ssssww1135@gmail.com>
2022-09-14 17:11:40 +0900

    Hello 클래스에 message 멤버 변수 추가

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -2,8 +2,10 @@ package git_test2;
 
 public class Hello {
 
-	public void say() {
-        System.out.println("안녕하세요");
+    String message = "안녕하세요";
+
+    public void say() {
+        Sysyem.out.println(message);
     }
 
 }
