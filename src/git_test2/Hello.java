f954f1dd507f3279d8d8cf264bff28731546531d
Seong, Hyunwoo <ssssww1135@gmail.com>
2022-09-19 17:04:26 +0900

    Hello 클래스 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -2,8 +2,18 @@ package git_test2;
 
 public class Hello {
 
-	public void say() {
-        System.out.println("안녕하세요");
+    String message;
+
+    public Hello() {
+        this("안녕하세요");
+    }
+
+    public Hello(String message) {
+        this.message = message;
+    }
+
+    public void say() {
+        System.out.println(message);
     }
 
 }
